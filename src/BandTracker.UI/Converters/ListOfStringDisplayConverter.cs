ae0c602d4de3a0c1a71d3423a10a9c8b48003403
Michal Diviš <A408674@dekra.org>
2022-08-18 10:08:43 +0200

    fixed genre separators

---
+++ src/BandTracker.UI/Converters/ListOfStringDisplayConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace BandTracker.UI.Converters;
+
+public class ListOfStringDisplayConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not List<string> items)
+        {
+            return value;
+        }
+
+        return string.Join(", ", items);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
